886cde514e2255cf380b64cd0b20a7578564810f
Chaitanya Garg <chaitanya.gargsg@gmail.com>
2022-08-16 14:04:01 +0530

    first commit:

---
+++ 210050039_inlab1/script.js
@@ -0,0 +1,9 @@
+function color_red(){
+	document.getElementById("para").style.color="red";
+}
+function color_blue(){
+	document.getElementById("para").style.color="blue";
+}
+function popup(){
+	alert("Hi "+ document.getElementById("name").value + ", have a nice day.");
+}
